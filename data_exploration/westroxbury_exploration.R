867d3a16ed04e992044ae84732e6c66502d99636
Derrick <dstuckey@lotame.com>
2019-11-16 14:29:18 -0500

    more and more plot types

--- data_exploration/westroxbury_exploration.R
+++ data_exploration/westroxbury_exploration.R
@@ -6,7 +6,9 @@ westroxbury <- read.csv("./WestRoxbury.csv")
 
 head(westroxbury)
 
-hist(westroxbury$TOTAL.VALUE)
+# ggplot histogram of total value
+ggplot(data=westroxbury) + 
+  geom_histogram(mapping=aes(x=TOTAL.VALUE))
 
 # simple scatter plot of total value vs square footage
 ggplot(data=westroxbury) + 
@@ -15,6 +17,10 @@ ggplot(data=westroxbury) +
 # add a dimension
 ggplot(data=westroxbury) + 
   geom_point(mapping = aes(x=LOT.SQFT, y=TOTAL.VALUE, color=REMODEL))
+
+# look at distribution of values for "REMODEL" alone
+ggplot(data=westroxbury) + 
+  geom_bar(mapping=aes(x=REMODEL))
 
 # too crowded in some areas; take a random sample
 nrow(westroxbury)
@@ -41,7 +47,40 @@ ggplot(data=roxbury.sample) +
   geom_point(mapping = aes(x=LOT.SQFT, y=TOTAL.VALUE)) + 
   facet_wrap(~ REMODEL, nrow=2)
 
+# try geom_smooth to show a fitted curve
 ggplot(data=roxbury.sample) + 
+  geom_smooth(mapping = aes(x=LOT.SQFT, y=TOTAL.VALUE))
+
+# show the fitted curve AND the actual points
+ggplot(data=roxbury.sample) + 
+  geom_smooth(mapping = aes(x=LOT.SQFT, y=TOTAL.VALUE)) + 
   geom_point(mapping = aes(x=LOT.SQFT, y=TOTAL.VALUE))
 
+
+
+
+
+
+
+# add another variable, BEDROOMS, into the mix
+
+# distribution of values for REMODEL variable
+# with number of bedrooms shaded
+ggplot(data=westroxbury) + 
+  geom_bar(mapping=aes(x=REMODEL, fill=BEDROOMS))
+
+# why doesn't this work?
+summary(westroxbury$BEDROOMS)
+is.factor(westroxbury$BEDROOMS)
+
+# make BEDROOMS variable a factor, and try again
+westroxbury$BEDROOMS <- as.factor(westroxbury$BEDROOMS)
+summary(westroxbury$BEDROOMS)
+ggplot(data=westroxbury) + 
+  geom_bar(mapping=aes(x=REMODEL, fill=BEDROOMS))
+
+# one more way to view the same data
+ggplot(data=westroxbury) + 
+  geom_bar(mapping=aes(x=REMODEL, fill=BEDROOMS),
+           position="dodge")
 
