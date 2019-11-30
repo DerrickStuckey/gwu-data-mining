9498eb730b8e14cf7d3d088b2118b6dcaac5e186
Derrick <dstuckey@lotame.com>
2019-11-30 10:13:59 -0500

    performance against a holdout set for tiny models

--- linear_regression/diamonds_polynomial.R
+++ linear_regression/diamonds_polynomial.R
@@ -38,6 +38,8 @@ ggplot(data=train.data[train.data.sample.idx,]) +
   geom_abline(intercept=0, slope=1, color="red")
 # coord_equal ensures the x and y scales are the same
 
+# note: some price predictions are less than 0 - why is this?
+
 # compare with the plot of price vs carat directly
 ggplot(data=train.data[train.data.sample.idx,]) + 
   geom_point(mapping = aes(x=carat,y=price))
@@ -121,103 +123,142 @@ ggplot(data=train.data.tiny) +
   geom_point(mapping = aes(x = carat, y = price))
 
 # train our 3 polynomial models on the tiny dataset
-tiny.lm.1 <- lm(data=train.data.tiny, price ~ carat)
-summary(tiny.lm.1)
+tiny.poly.1 <- lm(data=train.data.tiny, price ~ carat)
+summary(tiny.poly.1)
 
-tiny.lm.2 <- lm(data=train.data.tiny, price ~ carat + I(carat^2))
-summary(tiny.lm.2)
+tiny.poly.2 <- lm(data=train.data.tiny, price ~ carat + I(carat^2))
+summary(tiny.poly.2)
 
-tiny.lm.3 <- lm(data=train.data.tiny, price ~ carat + I(carat^2) + I(carat^3))
-summary(tiny.lm.3)
+tiny.poly.3 <- lm(data=train.data.tiny, price ~ carat + I(carat^2) + I(carat^3))
+summary(tiny.poly.3)
 
+tiny.poly.5 <- lm(data=train.data.tiny, price ~ carat + I(carat^2) + I(carat^3)
+                  + I(carat^4) + I(carat^5))
+summary(tiny.poly.5)
+
+tiny.poly.8 <- lm(data=train.data.tiny, price ~ carat + I(carat^2) + I(carat^3)
+                  + I(carat^4) + I(carat^5) + I(carat^6) + I(carat^7)+ I(carat^8))
+summary(tiny.poly.8)
 # look at the curves fit by each model
 
 # simple linear model
-tiny.lm.1$coefficients
+tiny.poly.1$coefficients
 ggplot(data=train.data.tiny) + 
   geom_point(mapping = aes(x = carat, y = price)) + 
   stat_function(geom = "line",
-                fun = function(x) tiny.lm.1$coefficients[1] + 
-                  tiny.lm.1$coefficients[2]*x
+                fun = function(x) tiny.poly.1$coefficients[1] + 
+                  tiny.poly.1$coefficients[2]*x
                 )
 
 
 # 2nd-order polynomial
-tiny.lm.2$coefficients
+tiny.poly.2$coefficients
 ggplot(data=train.data.tiny) + 
   geom_point(mapping = aes(x = carat, y = price)) + 
   stat_function(geom = "line",
-                fun = function(x) tiny.lm.2$coefficients[1] + 
-                  tiny.lm.2$coefficients[2]*x + 
-                  tiny.lm.2$coefficients[3]*x^2
+                fun = function(x) tiny.poly.2$coefficients[1] + 
+                  tiny.poly.2$coefficients[2]*x + 
+                  tiny.poly.2$coefficients[3]*x^2
   )
 
 # 3rd-order polynomial
-tiny.lm.3$coefficients
+tiny.poly.3$coefficients
 ggplot(data=train.data.tiny) + 
   geom_point(mapping = aes(x = carat, y = price)) + 
   stat_function(geom = "line",
-                fun = function(x) tiny.lm.3$coefficients[1] + 
-                  tiny.lm.3$coefficients[2]*x + 
-                  tiny.lm.3$coefficients[3]*x^2 + 
-                  tiny.lm.3$coefficients[4]*x^3
+                fun = function(x) tiny.poly.3$coefficients[1] + 
+                  tiny.poly.3$coefficients[2]*x + 
+                  tiny.poly.3$coefficients[3]*x^2 + 
+                  tiny.poly.3$coefficients[4]*x^3
   )
 
 
 # 5th-order polynomial
-tiny.lm.5 <- lm(data=train.data.tiny, price ~ carat + I(carat^2) + I(carat^3)
-                + I(carat^4) + I(carat^5))
-summary(tiny.lm.5)
-
-tiny.lm.5$coefficients
+tiny.poly.5$coefficients
 ggplot(data=train.data.tiny) + 
   geom_point(mapping = aes(x = carat, y = price)) + 
   stat_function(geom = "line",
-                fun = function(x) tiny.lm.5$coefficients[1] + 
-                  tiny.lm.5$coefficients[2]*x + 
-                  tiny.lm.5$coefficients[3]*x^2 + 
-                  tiny.lm.5$coefficients[4]*x^3 + 
-                  tiny.lm.5$coefficients[5]*x^4 + 
-                  tiny.lm.5$coefficients[6]*x^5
+                fun = function(x) tiny.poly.5$coefficients[1] + 
+                  tiny.poly.5$coefficients[2]*x + 
+                  tiny.poly.5$coefficients[3]*x^2 + 
+                  tiny.poly.5$coefficients[4]*x^3 + 
+                  tiny.poly.5$coefficients[5]*x^4 + 
+                  tiny.poly.5$coefficients[6]*x^5
   )
 
 
 # 8th-order polynomial
-tiny.lm.8 <- lm(data=train.data.tiny, price ~ carat + I(carat^2) + I(carat^3)
-                + I(carat^4) + I(carat^5) + I(carat^6) + I(carat^7)+ I(carat^8))
-summary(tiny.lm.8)
-
-tiny.lm.8$coefficients
+tiny.poly.8$coefficients
 ggplot(data=train.data.tiny) + 
   geom_point(mapping = aes(x = carat, y = price)) + 
   stat_function(geom = "line",
-                fun = function(x) tiny.lm.8$coefficients[1] + 
-                  tiny.lm.8$coefficients[2]*x + 
-                  tiny.lm.8$coefficients[3]*x^2 + 
-                  tiny.lm.8$coefficients[4]*x^3 + 
-                  tiny.lm.8$coefficients[5]*x^4 + 
-                  tiny.lm.8$coefficients[6]*x^5 + 
-                  tiny.lm.8$coefficients[7]*x^6 + 
-                  tiny.lm.8$coefficients[8]*x^7 + 
-                  tiny.lm.8$coefficients[9]*x^8
+                fun = function(x) tiny.poly.8$coefficients[1] + 
+                  tiny.poly.8$coefficients[2]*x + 
+                  tiny.poly.8$coefficients[3]*x^2 + 
+                  tiny.poly.8$coefficients[4]*x^3 + 
+                  tiny.poly.8$coefficients[5]*x^4 + 
+                  tiny.poly.8$coefficients[6]*x^5 + 
+                  tiny.poly.8$coefficients[7]*x^6 + 
+                  tiny.poly.8$coefficients[8]*x^7 + 
+                  tiny.poly.8$coefficients[9]*x^8
   )
 
 # check out the validation results
-val.preds.tiny.lm.1 <- predict(tiny.lm.1, newdata = validation.data)
+val.preds.tiny.poly.1 <- predict(tiny.poly.1, newdata = validation.data)
 ggplot() +
-  geom_point(mapping = aes(x=val.preds.tiny.lm.1, y=validation.data$price)) +
-  coord_equal(ratio=1)
+  geom_point(mapping = aes(x=val.preds.tiny.poly.1, y=validation.data$price)) +
+  coord_equal(ratio=1) + 
+  geom_abline(intercept=0, slope=1, color="red") + 
+  ggtitle("Tiny Polynomial Model 1")
 
-val.preds.tiny.lm.2 <- predict(tiny.lm.2, newdata = validation.data)
-plot(val.preds.tiny.lm.2,validation.data$price)
+val.preds.tiny.poly.2 <- predict(tiny.poly.2, newdata = validation.data)
+ggplot() +
+  geom_point(mapping = aes(x=val.preds.tiny.poly.2, y=validation.data$price)) +
+  coord_equal(ratio=1) + 
+  geom_abline(intercept=0, slope=1, color="red") + 
+  ggtitle("Tiny Polynomial Model 2")
 
-val.preds.tiny.lm.3 <- predict(tiny.lm.3, newdata = validation.data)
-plot(val.preds.tiny.lm.3,validation.data$price)
+val.preds.tiny.poly.3 <- predict(tiny.poly.3, newdata = validation.data)
+ggplot() +
+  geom_point(mapping = aes(x=val.preds.tiny.poly.3, y=validation.data$price)) +
+  coord_equal(ratio=1) + 
+  geom_abline(intercept=0, slope=1, color="red") + 
+  ggtitle("Tiny Polynomial Model 3")
 
-val.preds.tiny.lm.5 <- predict(tiny.lm.5, newdata = validation.data)
-plot(val.preds.tiny.lm.5,validation.data$price)
+val.preds.tiny.poly.5 <- predict(tiny.poly.5, newdata = validation.data)
+ggplot() +
+  geom_point(mapping = aes(x=val.preds.tiny.poly.5, y=validation.data$price)) +
+  geom_abline(intercept=0, slope=1, color="red") + 
+  ggtitle("Tiny Polynomial Model 5")
 
-val.preds.tiny.lm.8 <- predict(tiny.lm.8, newdata = validation.data)
-plot(val.preds.tiny.lm.8,validation.data$price)
+val.preds.tiny.poly.8 <- predict(tiny.poly.8, newdata = validation.data)
+ggplot() +
+  geom_point(mapping = aes(x=val.preds.tiny.poly.8, y=validation.data$price)) +
+  geom_abline(intercept=0, slope=1, color="red") + 
+  ggtitle("Tiny Polynomial Model 8")
 
+# some predictions WAY off for the higher-order models - why is this?
+
+# compare accuracy measures across models
+library(forecast)
+accuracy(val.preds.tiny.poly.1, validation.data$price)
+accuracy(val.preds.tiny.poly.2, validation.data$price)
+accuracy(val.preds.tiny.poly.3, validation.data$price)
+accuracy(val.preds.tiny.poly.5, validation.data$price)
+accuracy(val.preds.tiny.poly.8, validation.data$price)
+
+# ME: Mean Error
+# RMSE: Root Mean Squared Error
+# MAE: Mean Absolute Error
+# MPE: Mean Percentage Error
+# MAPE: Mean Absolute Percentage Error
+
+# what about R-squared?
+cor(val.preds.tiny.poly.1, validation.data$price) ^ 2
+cor(val.preds.tiny.poly.2, validation.data$price) ^ 2
+cor(val.preds.tiny.poly.3, validation.data$price) ^ 2
+cor(val.preds.tiny.poly.5, validation.data$price) ^ 2
+cor(val.preds.tiny.poly.8, validation.data$price) ^ 2
+
+# what about adjusted R-squared?
 
