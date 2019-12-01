0212954f4b40129b4ca37d7b78861f1d99e8dfef
Derrick <dstuckey@lotame.com>
2019-12-01 09:39:36 -0500

    over 100 iterations, stepwise wins

--- linear_regression/mtcars_scratch.R
+++ linear_regression/mtcars_scratch.R
@@ -4,30 +4,41 @@ mtcars
 head(mtcars)
 dim(mtcars)
 
-sample(1:10,5)
-
+set.seed(12345)
 train.proportion <- 0.75
-train.idx <- sample(1:nrow(mtcars), nrow(mtcars)*train.proportion)
-train.idx
 
-train.data <- mtcars[train.idx,]
-test.data <- mtcars[-train.idx,]
+# arrays to store the R-squared scores for each run
+full.scores <- c()
+step.scores <- c()
 
-nrow(train.data)
-nrow(test.data)
+# run the regressions 100 times, each with a different train/test split
+for (i in 1:100) {
+  train.idx <- sample(1:nrow(mtcars), nrow(mtcars)*train.proportion)
+  train.idx
+  
+  train.data <- mtcars[train.idx,]
+  test.data <- mtcars[-train.idx,]
+  
+  nrow(train.data)
+  nrow(test.data)
+  
+  full.lm <- lm(mpg ~ ., data = train.data)
+  summary(full.lm)
+  
+  step.lm <- step(full.lm, direction = "backward")
+  summary(step.lm)
+  
+  test.data$preds.full.lm <- predict(full.lm, newdata = test.data)
+  test.data$preds.step.lm <- predict(step.lm, newdata = test.data)
+  
+  # append the R-squared result for each model to the result arrays
+  full.scores <- c(full.scores, cor(test.data$preds.full.lm, test.data$mpg) ^ 2)
+  step.scores <- c(step.scores, cor(test.data$preds.step.lm, test.data$mpg) ^ 2)
+}
 
-full.lm <- lm(mpg ~ ., data = train.data)
-summary(full.lm)
+# compare the results
+mean(full.scores)
+mean(step.scores)
 
-step.lm <- step(full.lm, direction = "backward")
-summary(step.lm)
-
-test.data$preds.full.lm <- predict(full.lm, newdata = test.data)
-test.data$preds.step.lm <- predict(step.lm, newdata = test.data)
-
-cor(test.data$preds.full.lm, test.data$mpg) ^ 2
-cor(test.data$preds.step.lm, test.data$mpg) ^ 2
-
-# WTF! stepwise regression fails again vs. a model using all predictors!
-
-
+median(full.scores)
+median(step.scores)
