a910d59b54e55b7fd79965a316128595685b36c7
Derrick <dstuckey@lotame.com>
2019-12-01 17:33:39 -0500

    basic decision tree model on kaggle titanic data

--- trees/titanic_trees.R
+++ trees/titanic_trees.R
@@ -1,12 +1,50 @@
+library(rpart)
+library(rpart.plot)
 
 # from https://www.kaggle.com/c/titanic/data
-titanic.train <- read_csv("./data/titanic/train.csv")
-titanic.train
-
-# View(titanic.train)
+# see link for data dictionary
+titanic.data <- read_csv("./data/titanic/train.csv")
+titanic.data
+dim(titanic.data)
 
 # 'Survived' should be a logical variable
-titanic.train$Survived <- as.logical(titanic.train$Survived)
+titanic.data$Survived <- as.logical(titanic.data$Survived)
 
+# partition into training and test sets
+set.seed(12345)
+train.proportion <- 0.75
+val.proportion <- 0.25
+
+train.index <- sample(1:nrow(titanic.data), nrow(titanic.data)*train.proportion)
+train.data <- titanic.data[train.index,]
+validation.data <- titanic.data[-train.index,]
+
+# data exploration
+View(train.data)
+
+# verify which variables are ready to use directly
+table(train.data$Survived)
+table(train.data$Pclass)
+length(unique(train.data$Name))
+table(train.data$Sex)
+summary(train.data$Age)
+table(train.data$SibSp)
+table(train.data$Parch)
+length(unique(train.data$Ticket))
+summary(train.data$Fare)
+length(unique(train.data$Cabin))
+length(unique(train.data$Embarked))
+
+# 'Age' is probably useful but has many missing values
+# come back to this later
+
+# train a classification tree to predict passenger survival
+# use only "tidy" variables
+# aka those which are either numeric or have a small number of classes
+surv.tree.1 <- rpart(Survived ~ Pclass + Sex + SibSp + Parch + Fare + Embarked,
+                     data=train.data,
+                     method="class")
+
+prp(surv.tree.1, type=1, extra=1, under=TRUE, split.font=2, varlen=-10)
 
 
