fa6fe11e789e6017265e261c14d7b4dc27ee0a29
Derrick <dstuckey@lotame.com>
2019-12-04 09:36:38 -0500

    cleanup

--- trees/titanic_trees.R
+++ trees/titanic_trees.R
@@ -118,10 +118,10 @@ ggplot() +
 table(validation.data$survival.probs.1)
 
 
+### Parameter Tuning ###
 
 # plot accuracy vs training, accuracy vs holdout against CP
 cps <- c(0.0001, 0.0002, 0.0005, 0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1)
-# cps <- c(0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1)
 balanced.accuracy.train <- c()
 balanced.accuracy.val <- c()
 
