588126d242a9b2ea82a8dbdefa5d05fc30e6f939
Derrick <dstuckey@lotame.com>
2019-12-15 15:35:05 -0500

    renamed models

--- kmeans/iris_kmeans.R
+++ kmeans/iris_kmeans.R
@@ -21,20 +21,20 @@ iris.numeric <- iris.tibble %>%
 # try running k-means with no normalization, and 2 clusters
 # (should be OK as our numeric variables are all of similar scale)
 set.seed(12345)
-km.1 <- kmeans(iris.numeric, 2)
+km.2 <- kmeans(iris.numeric, 2)
 
 # look at a summary of the results
-km.1
+km.2
 
 # what are the centers of each cluster that our k-means algo has found?
-km.1$centers
+km.2$centers
 
 # what cluster does it put the first few data points in?
-head(km.1$cluster)
+head(km.2$cluster)
 
 # add the cluster info to our dataframe
 # (as a factor so ggplot will know what to do with it)
-iris.numeric$cluster <- as.factor(km.1$cluster)
+iris.numeric$cluster <- as.factor(km.2$cluster)
 
 # visualize the clusters against Petal Size
 ggplot(data=iris.numeric) +
@@ -52,20 +52,20 @@ ggplot(data=iris.numeric) +
 
 # try again, with 3 clusters
 set.seed(12345)
-km.1 <- kmeans(iris.numeric, 3)
+km.3 <- kmeans(iris.numeric, 3)
 
 # look at a summary of the results
-km.1
+km.3
 
 # what are the centers of each cluster that our k-means algo has found?
-km.1$centers
+km.3$centers
 
 # what cluster does it put the first few data points in?
-head(km.1$cluster)
+head(km.3$cluster)
 
 # add the cluster info to our dataframe
 # (as a factor so ggplot will know what to do with it)
-iris.numeric$cluster <- as.factor(km.1$cluster)
+iris.numeric$cluster <- as.factor(km.3$cluster)
 
 # visualize the clusters against Petal Size
 ggplot(data=iris.numeric) +
@@ -80,7 +80,7 @@ ggplot(data=iris.numeric) +
   geom_point(mapping = aes(x=Petal.Length, y=Sepal.Length, col=cluster))
 
 # how well do the clusters it has found line up with the actual Species labels?
-table(km.1$cluster, iris.tibble$Species)
+table(km.3$cluster, iris.tibble$Species)
 
 
 # TODO try k-means with different numbers of iterations, visualizing the intermediate results after each iteration
