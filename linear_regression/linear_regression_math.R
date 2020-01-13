bb1734723a716e422a06817d0f41f543470a4ad4
Derrick <dstuckey@lotame.com>
2020-01-13 17:28:28 -0500

    added error measurements

--- linear_regression/linear_regression_math.R
+++ linear_regression/linear_regression_math.R
@@ -42,6 +42,15 @@ linear.model$coefficients
 linear.model$coefficients[1]
 linear.model$coefficients[2]
 
+  # aside: calculate the coefficients manually
+  xm <- mean(x)
+  ym <- mean(y)
+  b1.calc <- sum( (x-xm)*(y-ym) ) / sum( (x-xm)^2 )
+  b0.calc <- ym - b1.calc*xm
+  b0.calc
+  b1.calc
+  linear.model$coefficients
+
 b0.lm <- linear.model$coefficients[1]
 b1.lm <- linear.model$coefficients[2]
 ggplot() + 
@@ -53,13 +62,50 @@ ggplot() +
 
 # calculate SSE (sum of squared errors) for the "guess" model
 preds.guess <- b0.guess + b1.guess * x
-errors.guess <- preds.guess - y
+errors.guess <- y - preds.guess
 errors.guess
 sum.squared.errors.guess <- sum(errors.guess^2)
-squared.errors.guess
+sum.squared.errors.guess
 
 # calculate SSE for the linear model
 preds.lm <- b0.lm + b1.lm * x
-sum.squared.errors.lm <- sum((preds.lm - y)^2)
+errors.lm <- y - preds.lm
+sum.squared.errors.lm <- sum(errors.lm^2)
 sum.squared.errors.lm
+
+# which one is lower?
+
+
+# calculate RMSE, ME, MPE, MAPE, MAE
+
+# ME (mean error)
+me.lm <- mean(errors.lm)
+me.lm
+
+# RMSE (root mean squared error)
+mse.lm <- sum.squared.errors.lm / length(errors.lm)
+rmse.lm <- sqrt(mse.lm)
+rmse.lm
+
+# MAE (mean absolute error)
+abs.errors.lm <- abs(errors.lm)
+abs.errors.lm
+mae.lm <- mean(abs.errors.lm)
+
+# MPE (mean percentage error)
+mpe.lm <- mean(errors.lm / y) * 100
+mpe.lm
+
+# MAPE (mean absolute percentage error)
+mape.lm <- mean(abs.errors.lm / y) * 100
+mape.lm
+
+# or we can get these values much more easily from the accuracy() function
+# in the "forecast" package 
+library(forecast)
+accuracy(preds.lm, y)
+
+# compare with the "guess" values
+accuracy(preds.guess, y)
+# are the model predictions best on every measure?
 
