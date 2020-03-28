291ba914eefb1858071581d433c099ff10d2566b
derrickstuckey <dstuckey@lotame.com>
2020-03-28 11:39:37 -0400

    normalize

--- kmeans/iris_kmeans.R
+++ kmeans/iris_kmeans.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(stats) # for kmeans
+library(caret) # for normalizer
 
 # load the data
 iris
@@ -13,15 +14,26 @@ summary(iris.tibble$Sepal.Length)
 summary(iris.tibble$Sepal.Width)
 summary(iris.tibble$Petal.Length)
 summary(iris.tibble$Petal.Width)
+sd(iris.tibble$Sepal.Length)
+sd(iris.tibble$Sepal.Width)
+sd(iris.tibble$Petal.Length)
+sd(iris.tibble$Petal.Width)
 
 # pull out just the numeric variables to run our k-means algo against
 iris.numeric <- iris.tibble %>%
   select(Sepal.Length, Sepal.Width, Petal.Length, Petal.Width)
 
-# try running k-means with no normalization, and 2 clusters
+# normalize the numeric data
+# (we could probably skip this as the variables are of similar scale)
+normalizer <- preProcess(iris.numeric,
+                         method = c("center", "scale"))
+
+iris.normalized <- predict(normalizer, iris.numeric)
+
+# try running k-means on the normalized numeric data
 # (should be OK as our numeric variables are all of similar scale)
 set.seed(12345)
-km.2 <- kmeans(iris.numeric, 2)
+km.2 <- kmeans(iris.normalized, 2)
 
 # look at a summary of the results
 km.2
@@ -52,7 +64,7 @@ ggplot(data=iris.numeric) +
 
 # try again, with 3 clusters
 set.seed(12345)
-km.3 <- kmeans(iris.numeric, 3)
+km.3 <- kmeans(iris.normalized, 3)
 
 # look at a summary of the results
 km.3
@@ -80,6 +92,7 @@ ggplot(data=iris.numeric) +
   geom_point(mapping = aes(x=Petal.Length, y=Sepal.Length, col=cluster))
 
 # how well do the clusters it has found line up with the actual Species labels?
+# (this wasn't explicitly our goal though, as we didn't use the Species labels for clustering)
 table(km.3$cluster, iris.tibble$Species)
 
 
