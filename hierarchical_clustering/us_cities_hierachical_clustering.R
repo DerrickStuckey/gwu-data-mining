2635e0dc8820b44a25f9d7da47d9a482e6f4cf73
derrickstuckey <dstuckey@lotame.com>
2020-03-28 13:20:55 -0400

    added Centroid, Ward methods

--- hierarchical_clustering/us_cities_hierachical_clustering.R
+++ hierarchical_clustering/us_cities_hierachical_clustering.R
@@ -62,10 +62,8 @@ ggplot(data=top.50.avg.clusters) +
 ggsave(paste(chart.dir,"/average_clustering.png",sep=""))
 
 
-# perform clustering using single cluster distance
+# perform clustering using the 'Single' distance method
 hc.single <- hclust(dist.euclid, method="single")
-
-# plot(hc.single)
 
 # split into 5 clusters
 hc.single.clusters.5 <- cutree(hc.single, k=5)
@@ -74,16 +72,17 @@ hc.single.clusters.5 <- cutree(hc.single, k=5)
 top.50.single.clusters <- top.50.coords
 top.50.single.clusters$cluster <- as.factor(hc.single.clusters.5)
 
+# plot the 5 clusters using the 'Single' distance method
 ggplot(data=top.50.single.clusters) + 
   geom_point(mapping = aes(x=longitude, y=latitude, col=cluster, shape=cluster)) + 
   ggtitle("Method: Single")
 ggsave(paste(chart.dir,"/single_clustering.png",sep=""))
 
+# how is this different from the "Average" method?
+
 
 # perform clustering using complete cluster distance
 hc.complete <- hclust(dist.euclid, method="complete")
-
-# plot(hc.complete)
 
 # split into 5 clusters
 hc.complete.clusters.5 <- cutree(hc.complete, k=5)
@@ -98,4 +97,34 @@ ggplot(data=top.50.complete.clusters) +
 ggsave(paste(chart.dir,"/complete_clustering.png",sep=""))
 
 
-# TODO try other agglomeration methods e.g. centroid, Ward
+# try 'Centroid' distance
+hc.centroid <- hclust(dist.euclid, method="centroid")
+
+# split into 5 clusters
+hc.centroid.clusters.5 <- cutree(hc.centroid, k=5)
+
+# create a new dataframe to plot this particular clustering
+top.50.centroid.clusters <- top.50.coords
+top.50.centroid.clusters$cluster <- as.factor(hc.centroid.clusters.5)
+
+ggplot(data=top.50.centroid.clusters) + 
+  geom_point(mapping = aes(x=longitude, y=latitude, col=cluster, shape=cluster)) + 
+  ggtitle("Method: Centroid")
+ggsave(paste(chart.dir,"/centroid_clustering.png",sep=""))
+
+
+# try 'Ward' distance
+hc.ward <- hclust(dist.euclid, method="ward.D")
+
+# split into 5 clusters
+hc.ward.clusters.5 <- cutree(hc.ward, k=5)
+
+# create a new dataframe to plot this particular clustering
+top.50.ward.clusters <- top.50.coords
+top.50.ward.clusters$cluster <- as.factor(hc.ward.clusters.5)
+
+ggplot(data=top.50.ward.clusters) + 
+  geom_point(mapping = aes(x=longitude, y=latitude, col=cluster, shape=cluster)) + 
+  ggtitle("Method: Ward")
+ggsave(paste(chart.dir,"/ward_clustering.png",sep=""))
+
