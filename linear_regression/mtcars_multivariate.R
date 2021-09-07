3194bd9aa11a319bdca601c90aa38ad840cf3330
derrickstuckey <dstuckey@lotame.com>
2021-09-07 19:33:55 -0400

    simple multivariate regression model

---
+++ linear_regression/mtcars_multivariate.R
@@ -0,0 +1,68 @@
+library(tidyverse)
+
+# for 'accuracy' function
+library(forecast)
+
+# 'mtcars' dataset included with base R
+mtcars
+
+head(mtcars)
+dim(mtcars)
+
+set.seed(12345)
+train.proportion <- 0.75
+
+train.idx <- sample(1:nrow(mtcars), nrow(mtcars)*train.proportion)
+train.idx
+
+# train/test split
+train.data <- mtcars[train.idx,]
+test.data <- mtcars[-train.idx,]
+dim(train.data)
+dim(test.data)
+
+summary(mtcars$mpg)
+
+# train a linear model on 'cyl' only
+first.lm <- lm(mpg ~ cyl, data = train.data)
+summary(first.lm)
+
+# train a linear model on 'cyl' and 'disp'
+second.lm <- lm(mpg ~ cyl + disp, data = train.data)
+summary(second.lm)
+
+# train a linear model on all available predictors
+full.lm <- lm(mpg ~ ., data = train.data)
+summary(full.lm)
+
+# evaluate the model's performance against the training data
+preds.train <- predict(full.lm, newdata = train.data)
+accuracy(preds.train, train.data$mpg)
+
+# evaluate the model's performance against the test data
+preds.test <- predict(full.lm, newdata = test.data)
+accuracy(preds.test, test.data$mpg)
+
+# plot actual vs predicted values for the training data
+ggplot() +
+  geom_point(mapping = aes(x=preds.train, y=train.data$mpg))
+
+# add a line that represents perfect predictions (y=x)
+ggplot() +
+  geom_point(mapping = aes(x=preds.train, y=train.data$mpg)) + 
+  geom_abline(mapping = aes(intercept = 0, slope = 1), col="blue")
+
+# plot actual vs predicted values for the test data
+ggplot() +
+  geom_point(mapping = aes(x=preds.test, y=test.data$mpg)) + 
+  geom_abline(mapping = aes(intercept = 0, slope = 1), col="blue")
+
+# distribution of residuals for training data
+ggplot() +
+  geom_histogram(mapping = aes(train.data$mpg - preds.train), bins=8)
+
+# distribution of residuals for test data
+ggplot() +
+  geom_histogram(mapping = aes(test.data$mpg - preds.test), bins=8)
+
+
