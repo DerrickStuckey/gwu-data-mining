8b856463fe6958fb75098799f856a72a7cfe2bb9
derrickstuckey <dstuckey@lotame.com>
2021-11-04 07:09:31 -0400

    added SSE

--- kmeans/kmeans_stability_example.R
+++ kmeans/kmeans_stability_example.R
@@ -3,7 +3,7 @@ library(stats) # for kmeans
 library(caret) # for normalizer
 
 # load the data
-# from https://docs.google.com/spreadsheets/d/1zVbakwp2gQ8b0MpYcHBt8ykMA3x938z-RfwMHX5_LpI/edit?usp=sharing
+# from https://docs.google.com/spreadsheets/d/1rhUJJ40zsQuGFk5_vxbLmN-cJCbaSkWb4YTyHtBoXZY/edit?usp=sharing
 example.data <- read.csv("./kmeans/K-means clustering example - data.csv")
 
 # don't normalize in this case
@@ -13,8 +13,7 @@ ggplot(data=example.data) +
   geom_point(mapping = aes(x=x, y=y))
 # what do you think the 3 clusters will turn out to be?
 
-# seed.val <- 12345
-seed.val <- 67890
+seed.val <- 12345
 
 set.seed(seed.val)
 km.3a <- kmeans(example.data, 
@@ -22,12 +21,32 @@ km.3a <- kmeans(example.data,
 
 # add the cluster info to our dataframe
 # (as a factor so ggplot will know what to do with it)
-example.data$cluster <- as.factor(km.3a$cluster)
+example.data$cluster.a <- as.factor(km.3a$cluster)
 
 # visualize the clusters against x and y
 ggplot(data=example.data) +
-  geom_point(mapping = aes(x=x, y=y, col=cluster,
-                           shape=cluster)) +
+  geom_point(mapping = aes(x=x, y=y, col=cluster.a,
+                           shape=cluster.a)) +
   ggtitle(paste("Seed:",seed.val))
 
+km.3a$tot.withinss
+
+# try again
+seed.val <- 67890
+
+set.seed(seed.val)
+km.3b <- kmeans(example.data, 
+                centers=3)
+
+# add the cluster info to our dataframe
+# (as a factor so ggplot will know what to do with it)
+example.data$cluster.b <- as.factor(km.3b$cluster)
+
+# visualize the clusters against x and y
+ggplot(data=example.data) +
+  geom_point(mapping = aes(x=x, y=y, col=cluster.b,
+                           shape=cluster.b)) +
+  ggtitle(paste("Seed:",seed.val))
+
+km.3b$tot.withinss
 
