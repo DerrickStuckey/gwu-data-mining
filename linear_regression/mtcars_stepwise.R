df5df589efe4f1d052939aefbfc6086dda839857
Derrick Stuckey <derrickstuckey@gmail.com>
2022-09-14 18:41:34 -0400

    more performance results

--- linear_regression/mtcars_stepwise.R
+++ linear_regression/mtcars_stepwise.R
@@ -70,6 +70,7 @@ ggplot(train.data, mapping = aes(x=hp, y=mpg)) +
               formula=y ~ poly(x, 2, raw=TRUE), colour="blue")
 # any problems you can envision with this approach?
 
+
 # add the polynomial term to the full model
 full.hp.poly.lm <- lm(mpg ~ . - hp + poly(hp,2), data = train.data)
 summary(full.hp.poly.lm)
@@ -91,7 +92,13 @@ inter.lm.step <- step(base.lm, direction = "forward",
                       scope=list(lower=base.lm, upper=inter.lm.full))
 summary(inter.lm.step)
 
-# evaluate each against the test dataset
+
+
+
+
+## appendix
+
+# evaluate each model against the test dataset
 preds.inter.lm.1 <- predict(inter.lm.1, newdata = test.data)
 accuracy(preds.inter.lm.1, test.data$mpg)
 
@@ -103,3 +110,53 @@ accuracy(preds.inter.lm.2, test.data$mpg)
 
 preds.inter.lm.step <- predict(inter.lm.step, newdata = test.data)
 accuracy(preds.inter.lm.step, test.data$mpg)
+
+# evaluate each against the training dataset for comparison
+train.preds.full.lm <- predict(full.lm, newdata = train.data)
+accuracy(train.preds.full.lm, train.data$mpg)
+
+train.preds.step.lm.back <- predict(step.lm.back, newdata = train.data)
+accuracy(train.preds.step.lm.back, train.data$mpg)
+
+train.preds.inter.lm.1 <- predict(inter.lm.1, newdata = train.data)
+accuracy(train.preds.inter.lm.1, train.data$mpg)
+
+train.preds.full.hp.poly.lm <- predict(full.hp.poly.lm, newdata = train.data)
+accuracy(train.preds.full.hp.poly.lm, train.data$mpg)
+
+train.preds.inter.lm.2 <- predict(inter.lm.2, newdata = train.data)
+accuracy(train.preds.inter.lm.2, train.data$mpg)
+
+train.preds.inter.lm.step <- predict(inter.lm.step, newdata = train.data)
+accuracy(train.preds.inter.lm.step, train.data$mpg)
+
+
+# plot train and test with hp polynomial curve fit
+mtcars2 <- mtcars
+mtcars2$partition <- "test"
+mtcars2$partition[train.idx] <- "train"
+
+ggplot(mtcars2, mapping = aes(x=hp, y=mpg, col=partition)) +
+  geom_point() + 
+  stat_smooth(method="lm", se=TRUE, fill=NA,
+              formula=y ~ poly(x, 2, raw=TRUE))
+
+# compare performance of hp only model with poly(hp,2) model
+lm.hp <- lm(mpg ~ hp, data=train.data)
+summary(lm.hp)
+
+train.preds.hp <- predict(lm.hp, newdata = train.data)
+accuracy(train.preds.hp, train.data$mpg)
+
+test.preds.hp <- predict(lm.hp, newdata = test.data)
+accuracy(test.preds.hp, test.data$mpg)
+
+lm.hp.2 <- lm(mpg ~ poly(hp,2), data=train.data)
+summary(lm.hp.2)
+
+train.preds.hp.2 <- predict(lm.hp.2, newdata = train.data)
+accuracy(train.preds.hp.2, train.data$mpg)
+
+test.preds.hp.2 <- predict(lm.hp.2, newdata = test.data)
+accuracy(test.preds.hp.2, test.data$mpg)
+
